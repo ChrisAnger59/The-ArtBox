54bdd3da6c9c4d77f7e59129250133c9adc3bebc
Christophe <christophe.anger.pro@gmail.com>
2026-04-27 16:33:11 +0200

    recuperation oeuvres en bdd + ajout nouvelle oeuvre via formulaire

---
+++ traitement.php
@@ -0,0 +1,24 @@
+<?php
+require 'bdd.php';
+
+
+if(empty($_POST['titre']) 
+|| empty($_POST['artiste']) 
+|| empty($_POST['image']) 
+|| empty($_POST['description'])
+|| strlen($_POST['description']) < 3
+|| !filter_var($_POST['image'], FILTER_VALIDATE_URL)) {
+    header('location: ajouter.php');
+}else{
+    $bdd = connexion();
+    $query = 'INSERT INTO `oeuvres` (`titre`, `artiste`, `image`, `description`) VALUES (:titre, :artiste, :image, :description)';
+    $insertStatement = $bdd->prepare($query);
+    $insertStatement->execute([
+        'titre' => $_POST['titre'],
+        'artiste' => $_POST['artiste'],
+        'image' => $_POST['image'],
+        'description' => $_POST['description']
+    ]);
+
+    header('location: index.php');
+}
